cecf114e32a551323216112070a62b4c6299ed48
5rdenis <37657152+5rdenis@users.noreply.github.com>
2021-04-12 01:47:25 +0300

    Update task3.rb

--- task3/task3.rb
+++ task3/task3.rb
@@ -6,5 +6,5 @@ count_hash = Hash.new(0)
 arr.each { |x| count_hash[x] += 1 }
 puts count_hash
 
-#Оказывается теперь в ruby есть метод .tally созданный именно для такой задачи
+#Начиная с версии 2.7 в Ruby есть метод .tally созданный именно для такой задачи
 #puts arr.tally
